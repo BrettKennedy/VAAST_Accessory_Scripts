5625540c3966dcfa829159b88b96cdee1fc4da83
BrettKennedy <brett.kennedy@utah.edu>
2016-04-16 16:49:29 -0600

    this program has nothing to do with VAAST

--- probeCoverageBed.py
+++ probeCoverageBed.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import numpy as np
 import pandas as pd
+import operator
 
 def get_args():
     parser = argparse.ArgumentParser(description="takes a list of gatk "
@@ -11,33 +12,59 @@ def get_args():
                                      "depth of coverage across all samples, "
                                      "also contains columns for the coverage "
                                      "at that position for each sample")
-    parser.add_argument("summaries", nargs='+', help="comma seperated list"
+    parser.add_argument("summaries", nargs='+', help="space seperated list"
                         " of sample_interval_summary files")
-    parser.add_argument("output", help="name of output bed file")
+    parser.add_argument("--output", help="name of output bed file")
     return parser.parse_args()
 
 
-def parseSummary(summary, coverageFrame):
+def parseSummary(summary, coverageDict):
     """Parses the GATK generated sample_interval_summary into """
-    if coverageFrame:
+    if coverageDict.keys():
         with open(summary) as t:
             for line in csv.reader(t, delimiter="\t"):
-
+                if line[0] == "Target":
+                    continue
+                try:
+                    coverageDict[line[0]].append(float(line[2]))
+                except KeyError:
+                    print "Coverage files not created with the same bed"
     else:
         with open(summary) as t:
-            for line in csv.reader(t, delimiter="\t")
-
-    pass
+            for line in csv.reader(t, delimiter="\t"):
+                if line[0] == "Target":
+                    continue
+                coverageDict[line[0]] = [float(line[2])]
+    return 0
 
 
 def generateHeader(summaries, output):
     """ Writes out the header file by column to the output bed file """
-    pass
+    header = ["#chr", "region_start", "region_end", "mean_coverage"]
+    header.extend([i.strip("_newBed.sample_interval_summary").strip("\/")
+                   for i in summaries])
+    header.append("\n")
+    output.write("\t".join(header))
+    return 0
 
 
 def generateBed(summaries, output):
     """ Generates the bed files """
-    pass
+    coverageDict = {}
+    meanDict = {}
+    for summary in summaries:
+        parseSummary(summary, coverageDict)
+    for region in coverageDict.keys():
+        meanDict[region] = np.mean(coverageDict[region])
+    with open(output, 'w') as out:
+        generateHeader(summaries, out)
+        for region in sorted(meanDict, key=lambda k: meanDict[k]):
+            covReg = [str(i) for i in coverageDict[region]]
+            reg = "%s\t%s" % (region.split(':')[0], 
+                                  "\t".join(region.split(':')[1].split('-')))
+            
+            o = "%s\t%1.3f\t%s\n" % (reg, meanDict[region], "\t".join(covReg))
+            out.write(o)
 
 
 def main():
